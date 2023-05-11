946a175b4c5e8dc70792056eb93f66370dc24538
Jacky Tran <destretz@gmail.com>
2023-05-11 13:34:46 -0400

    randomly set favicon on app load

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -1,18 +1,37 @@
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import React, { useLayoutEffect } from 'react';
+import React, { useEffect, useLayoutEffect, useState } from 'react';
 import { HomePage } from './HomePage/HomePage';
 import { COLOURS } from './utils';
+import { Helmet } from 'react-helmet';
 
 export const App: React.FC = () => {
     useLayoutEffect(() => {
         document.body.style.backgroundColor = COLOURS.BACKGROUND;
     });
 
+    const [iconFileName, setIconFileName] = useState<string>();
+    useEffect(() => {
+        const IconFileNames = ['Meguru.ico', 'Ayase.ico'];
+        setIconFileName(
+            IconFileNames[Math.floor(Math.random() * IconFileNames.length)]
+        );
+    }, []);
+
     return (
         <Router basename={process.env.PUBLIC_URL}>
             <Switch>
                 <Route path="/" exact>
-                    <HomePage />
+                    <>
+                        <Helmet>
+                            <link
+                                rel="icon"
+                                type="image/png"
+                                href={iconFileName}
+                                sizes="16x16"
+                            />
+                        </Helmet>
+                        <HomePage />
+                    </>
                 </Route>
             </Switch>
         </Router>
