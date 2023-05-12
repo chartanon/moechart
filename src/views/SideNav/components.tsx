68a94b37c02ec7e873ed98953ce9dc78eeb5c786
Jacky Tran <destretz@gmail.com>
2023-05-12 02:07:17 -0400

    responsiveness

--- src/views/SideNav/components.tsx
+++ src/views/SideNav/components.tsx
@@ -1,5 +1,5 @@
 import styled, { css } from 'styled-components';
-import { Row, LabelFont, Button } from '../utils';
+import { Row, LabelFont, Button, COLOURS } from '../utils';
 
 export const LegendItemContainer = styled(Row)`
     padding: 7px 5px 7px 5px;
@@ -18,18 +18,19 @@ export const LegendLabel = styled(LabelFont)`
 export const LegendButton = styled(Button)<{ $isSelected: boolean }>`
     :hover {
         font-weight: bold;
+        box-shadow: 2px 2px 4px ${COLOURS.TEXT};
+        outline: 3px solid ${COLOURS.TEXT};
     }
 
     :active {
-        box-shadow: 2px 2px 4px white;
-        outline: 2px solid white;
+        outline: 2px solid ${COLOURS.TEXT};
+        box-shadow: 2px 2px 4px ${COLOURS.TEXT};
     }
 
     ${({ $isSelected }) =>
         $isSelected
             ? css`
-                  box-shadow: 2px 2px 4px white;
-                  outline: 2px solid white;
+                  outline: 3px solid ${COLOURS.TEXT};
               `
             : ''};
 `;
